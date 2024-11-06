db967a00c743b2dccedfdd608aac22eb2bf83f16
Chris Reid <chris@raygun6.com>
2024-11-06 13:03:48 -0800

    improve todo show ux

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                row = f"{index} - {item}"
+                print(row)
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
