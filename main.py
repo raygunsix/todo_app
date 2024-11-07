4192a41318432bada53fc39d4aca0781ded9b779
Chris Reid <chris@raygun6.com>
2024-11-07 13:08:09 -0800

    improve code readability

--- main.py
+++ main.py
@@ -6,19 +6,17 @@ while True:
         case 'add':
             todo = input("Enter a todo: ") + "\n"
 
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
-            
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)    
+
         case 'show':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             for index, item in enumerate(todos):
                 row = f"{index + 1} - {item.strip("\n")}"
