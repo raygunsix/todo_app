55d6c79db7628db40c79c09bcb4f6dc8be151737
Chris Reid <chris@raygun6.com>
2024-11-08 16:24:08 -0800

    improve error handling

--- main.py
+++ main.py
@@ -20,35 +20,49 @@ while True:
         for index, item in enumerate(todos):
             row = f"{index + 1} - {item.strip("\n")}"
             print(row)
+
     elif user_action.startswith("edit"):
-        number = int(user_action[5:])
-        number = number - 1
+        try:
+            number = int(user_action[5:])
+            number = number - 1
 
-        with open('todos.txt', 'r') as file:
-                todos = file.readlines()
-        
-        new_todo = input("Enter new todo: ") + "\n"
-        todos[number] = new_todo
+            with open('todos.txt', 'r') as file:
+                    todos = file.readlines()
+            
+            new_todo = input("Enter new todo: ") + "\n"
+            todos[number] = new_todo
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)  
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)  
+        except ValueError:
+            print("You command was not valid")
+            continue
+
     elif user_action.startswith("complete"):
-        number = int(user_action[9:])
-        with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        try:
+            number = int(user_action[9:])
+            with open('todos.txt', 'r') as file:
+                    todos = file.readlines()
 
-        index = number - 1
-        todo_to_remove = todos[index]
-        todos.pop(index)
+            index = number - 1
+            todo_to_remove = todos[index]
+            todos.pop(index)
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)  
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)  
 
-        message = f"Todo {todo_to_remove.strip('\n')} was removed"
-        print(message)
+            message = f"Todo {todo_to_remove.strip('\n')} was removed"
+            print(message)
+        except ValueError:
+            print("You command was not valid")
+            continue
+        except IndexError: 
+            print("There is no item with that number")
+            continue
         
     elif 'exit' in user_action:
         break
+
     else:
          print("The command is not valid")
 
