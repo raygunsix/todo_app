f0efc6312a2ec4a4f74c4b3b5033ba4cdddfe9fe
Chris Reid <chris@raygun6.com>
2024-11-12 17:41:55 -0800

    add window label and text input box

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 import functions
-import FreeSimpleGUI
+import FreeSimpleGUI as sg
 
-window = FreeSimpleGUI.Window('My To-Do App', layout=[""])
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo")
+
+window = sg.Window('My To-Do App', layout=[[label, input_box]])
 window.read()
 window.close()
