26904927bd54d2c4e10ed98e7320b3bb7320840d
Chris Reid <chris@raygun6.com>
2024-11-19 16:56:24 -0800

    add initial web ui

---
+++ web.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+
+st.title("My Todo App")
+
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder="Enter a todo: ")
